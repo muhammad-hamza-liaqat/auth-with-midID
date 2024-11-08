e4b695bc496d705c3ed691d5b24a1b0fd9e45719
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-11-09 00:07:35 +0500

    code setup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
+require("dotenv").config();
 
-import express from "express";
-import authRoutes from "./routes/auth.routes.js";
-import { initializeClient } from "./services/authService.js";
-
+const express = require("express");
 const app = express();
-app.use("/", authRoutes);
 
-initializeClient()
-  .then(() => {
-    console.log("OpenID client initialized");
-  })
-  .catch((error) => {
-    console.error("Failed to initialize OpenID client:", error);
-    process.exit(1);
-  });
+app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running at http://localhost:${process.env.PORT}`);
-});
+  console.log(`server is running at http://localhost:${process.env.PORT}`)
+})
--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-const authRoutes = express.Router()
-import * as authController from "../controller/auth.controller.js"
-
-authRoutes.get("/", authController.login)
-authRoutes.get("/callback", authController.handleCallback)
-
-export default authRoutes
