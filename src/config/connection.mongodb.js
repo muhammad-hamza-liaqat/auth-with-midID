ffc4afdd24b676ab1d1885f05dd3bf890968ad7b
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-11-11 10:43:31 +0500

    mongodb-client added

---
+++ src/config/connection.mongodb.js
@@ -0,0 +1,16 @@
+// db.js
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGO_URI;
+const dbName = process.env.MONGO_NAME;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectDB() {
+    if (!client.isConnected) {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    }
+    return client.db(dbName);
+}
+
+module.exports = connectDB;
